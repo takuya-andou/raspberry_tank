09380aa9774f9b3c195825aef7467310b9e6ff34
Takuya Ando <takuya0411@gmail.com>
2015-04-13 18:49:57 +0900

    コメントに反応
    します

--- mytank.py
+++ mytank.py
@@ -14,7 +14,7 @@ PIN_R2 = 9
 
 g_mode = 0
 g_percentage = 50
-
+count_w = 0
 GPIO.setFunction( PIN_L1, GPIO.PWM )
 GPIO.setFunction( PIN_L2, GPIO.PWM )
 GPIO.setFunction( PIN_R1, GPIO.PWM )
@@ -73,3 +73,54 @@ MotorDrive( PIN_R1, PIN_R2, 50 );
 webiopi.sleep( 1.0 )
 MotorDrive( PIN_L1, PIN_L2, 0 );
 MotorDrive( PIN_R1, PIN_R2, 0 );
+
+
+
+pat = re.compile("(((left)|(right))\\s*,\\s*(\\d+))")
+patw = re.compile("((w)|(W))")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.11.12', 8092))
+while 1:
+    data = client_socket.recv(512)
+    if data:
+        for m in pat.findall(data.decode("shift-jis")):
+            print m[1] + " | " + m[4]
+            if int(m[4]) < 0:
+                motor = 0
+            elif int(m[4]) > 100:
+                motor = 100
+            else:
+                motor = int(m[4]) 
+            if m[1]=="left":
+            	MotorDrive( PIN_L1, PIN_L2, g_percentage );
+            	MotorDrive( PIN_R1, PIN_R2, -g_percentage );
+            	webiopi.sleep( 1.0 )
+            	MotorDrive( PIN_L1, PIN_L2, 0 );
+            	MotorDrive( PIN_R1, PIN_R2, 0 );
+            if m[1]=="right":
+            	MotorDrive( PIN_L1, PIN_L2, -g_percentage );
+            	MotorDrive( PIN_R1, PIN_R2, g_percentage );
+            	webiopi.sleep( 1.0 )
+            	MotorDrive( PIN_L1, PIN_L2, 0 );
+            	MotorDrive( PIN_R1, PIN_R2, 0 );
+
+            #otorDrive( IN1PIN, IN2PIN, motor )
+
+        r = re.compile(r">(.+?)<\/chat")
+        returnlist = r.findall(data.decode("shift-jis"))
+
+        if returnlist:
+            print returnlist[0].encode('utf-8')
+            for m in patw.findall(returnlist[0]):
+                count_w+= 1
+        if count_w > 0:
+       		MotorDrive( PIN_L1, PIN_L2, 50 );
+        	MotorDrive( PIN_R1, PIN_R2, 50 );
+        	webiopi.sleep( 1.0 )
+       		MotorDrive( PIN_L1, PIN_L2, 0 );
+        	MotorDrive( PIN_R1, PIN_R2, 0 );
+        	count_w=0
+        print "w_count:", count_w
+        
+    client_socket.send(data)
+    data = ""
